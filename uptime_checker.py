132e65db289dd865849b0757aae8a6650488805f
Sonal Gajbhiye <sonalgajbhiye2001@gmail.com>
2026-07-18 06:54:47 +0530

    Wrap website check logic into a reusable function

--- uptime_checker.py
+++ uptime_checker.py
@@ -1,11 +1,11 @@
 import requests
 
-url = "https://www.google.com"
-response = requests.get(url)
+def check_website(url):
+    response = requests.get(url)
+    if response.status_code == 200:
+        print(f"{url} is UP")
+    else:
+        print(f"{url} is DOWN")
 
-if response.status_code ==200:
-    print(f"{url} is UP")
-else:
-    print(f"{url} is DOWN")
-
-
+check_website("https://www.google.com")
+check_website("https://www.github.com")
