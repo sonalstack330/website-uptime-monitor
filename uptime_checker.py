727dfb752eacce38f6e0f56447216e3bb92fe243
Sonal Gajbhiye <sonalgajbhiye2001@gmail.com>
2026-07-18 20:48:42 +0530

    Add email alerts when a website goes down

--- uptime_checker.py
+++ uptime_checker.py
@@ -1,12 +1,20 @@
 import requests
 import logging
 import time
+import smtplib
+from email.mime.text import MIMEText
+
+from Downloads.uptime_checker import EMAIL_SENDER
 
 logging.basicConfig(
     filename="uptime_log.txt",
     level=logging.INFO,
     format="%(asctime)s - %(message)s",
 )
+
+EMAIL_SENDER = "Sonalgajbhiye2001@gmail.com"
+EMAIL_PASSWORD = "setrtsltemtbhiak"  # your 16-char app password
+EMAIL_RECEIVER = "Sonalgajbhiye04@gmail.com"
 
 def check_website(url):
     try:
@@ -15,12 +23,26 @@ def check_website(url):
             message = f"{url} is UP"
         else:
             message = f"{url} is DOWN (status {response.status_code})"
+            send_email_alert(url)
     except requests.exceptions.RequestException:
         message = f"{url} is DOWN (no response)"
+        send_email_alert(url)
 
     print(message)
     logging.info(message)
 
+def send_email_alert(url):
+    msg = MIMEText(f"Alert: {url} appears to be DOWN.")
+    msg["Subject"] = f"Website Down: {url}"
+    msg["From"] = EMAIL_SENDER
+    msg["To"] = EMAIL_RECEIVER
+
+    with smtplib.SMTP("smtp.gmail.com", 587) as server:
+        server.starttls()
+        server.login(EMAIL_SENDER, EMAIL_PASSWORD)
+        server.sendmail(EMAIL_SENDER, EMAIL_RECEIVER, msg.as_string())
+
+    print(f"Email alert sent for {url}")
 
 websites = [
     "https://www.google.com",
