90fe472801eaf783b006c366258764b91093c7a3
Sonal Gajbhiye <sonalgajbhiye2001@gmail.com>
2026-07-18 06:48:46 +0530

    Add UP/DOWN status check based on response code

--- uptime_checker.py
+++ uptime_checker.py
@@ -2,4 +2,10 @@ import requests
 
 url = "https://www.google.com"
 response = requests.get(url)
-print(response.status_code)
+
+if response.status_code ==200:
+    print(f"{url} is UP")
+else:
+    print(f"{url} is DOWN")
+
+
